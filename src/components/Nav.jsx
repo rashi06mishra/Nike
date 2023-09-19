6ace5a75e8bdfd0f39c77c63e888fcc3a0827431
Rashi Mishra <rashi06mishra@gmail.com>
2023-09-19 11:10:57 +0530

    Hero-1

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,7 +16,7 @@ const Nav = () => {
             </a>
             { open && 
               <div className=''>
-              <ul className ={`flex-1 flex justify-center items-center gap-16 max-lg:hidden`}>
+              <ul className ={`lg:flex-1 lg:flex lg:justify-center lg:items-center lg:display-block justify-center gap-4`}>
                 {navLinks.map( (item) => (
                     <li key={item.label}>
                       <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray '>
