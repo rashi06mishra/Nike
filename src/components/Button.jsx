2f6dca2f427248c12b64ee9f9ed72c95c24e20eb
Rashi Mishra <rashi06mishra@gmail.com>
2023-09-21 02:48:00 +0530

    subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-const Button = ( {label, iconURL, backgroundColor, textColor, borderColor} ) => {
+const Button = ( {label, iconURL, backgroundColor, textColor, borderColor, fullWidth} ) => {
   return (
     <button 
     className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-l leading-none 
@@ -6,7 +6,8 @@ const Button = ( {label, iconURL, backgroundColor, textColor, borderColor} ) => 
       backgroundColor
       ?`${backgroundColor} ${textColor} ${borderColor}`
       : "bg-coral-red text-white border-coral-red"}
-     rounded-full `}>
+     rounded-full ${fullWidth && "w-full"}`}
+     >
         {label}
         {
           iconURL && <img src={iconURL}
